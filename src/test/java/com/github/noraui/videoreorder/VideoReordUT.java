d00d52e47c526a9c3948a916e977fd95b99b831a
sgrillon14 <sgrillon@hotmail.fr>
2018-02-01 22:33:59 +0100

    add videoreorder

---
+++ src/test/java/com/github/noraui/videoreorder/VideoReordUT.java
@@ -0,0 +1,13 @@
+package com.github.noraui.videoreorder;
+
+import org.junit.Test;
+
+public class VideoReordUT {
+
+    @Test
+    public void runVideoReord() throws Exception {
+        VideoReord mrc = new VideoReord();
+        mrc.main(null);
+    }
+
+}
